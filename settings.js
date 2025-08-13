c5f6b71c4fdcfdd9918bba6e6aae818347bd8276
khelegafreefire0283-dotcom <khelegafreefire0283@gmail.com>
2025-08-13 07:54:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'biki', // Your name
-  ownerNumber: '918967963081', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '919083065024', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
